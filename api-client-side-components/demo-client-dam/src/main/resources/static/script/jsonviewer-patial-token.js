71bd7ec4edc5f34839097510fff753cf571929b5
Claire JUILLARD <cjuillard@NX-DOMAIN.LAN>
2023-09-11 11:39:10 +0200

    ui demo-client-dam

--- api-client-side-components/demo-client-dam/src/main/resources/static/script/jsonviewer-patial-token.js
+++ api-client-side-components/demo-client-dam/src/main/resources/static/script/jsonviewer-patial-token.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
                 const body=getData("#body");
                 let jsonViewerHeader = new JSONViewer();    
                 let jsonViewerBody = new JSONViewer();
+                   document.querySelector("#json-header").appendChild(jsonViewerHeader.getContainer());                           
+    jsonViewerHeader.showJSON(JSON.parse(header), -1, -1);
+    document.querySelector("#json-body").appendChild(jsonViewerBody.getContainer());                            
+    jsonViewerBody.showJSON(JSON.parse(body), -1, -1); 
+     var x = document.getElementById("details");
+     x.style.visibility = "hidden";
                 
         });   
                      
@@ -17,13 +23,13 @@ function details() {
   var btn = document.getElementById("btnDetails");
   var x = document.getElementById("details");
   if (x.style.visibility === "hidden") {
-	  alert("Les informations qui vont être affichées sont remontées dans le navigateur uniquement à titre de démonstration. En production, elles ne doivent pas sortir pas du proxy (\'Relying Party\')")
+//	  alert("Les informations qui vont être affichées sont remontées dans le navigateur uniquement à titre de démonstration. En production, elles ne doivent pas sortir pas du proxy (\'Relying Party\')")
 	   btn.textContent="Masquer des détails techniques"
     x.style.visibility = "visible";
-    document.querySelector("#json-header").appendChild(jsonViewerHeader.getContainer());                           
-        		jsonViewerHeader.showJSON(JSON.parse(header), -1, -1);
-        		document.querySelector("#json-body").appendChild(jsonViewerBody.getContainer());                            
-                jsonViewerBody.showJSON(JSON.parse(body), -1, -1); 
+//    document.querySelector("#json-header").appendChild(jsonViewerHeader.getContainer());                           
+//    jsonViewerHeader.showJSON(JSON.parse(header), -1, -1);
+//    document.querySelector("#json-body").appendChild(jsonViewerBody.getContainer());                            
+//    jsonViewerBody.showJSON(JSON.parse(body), -1, -1); 
    
   } else {
     x.style.visibility = "hidden";
