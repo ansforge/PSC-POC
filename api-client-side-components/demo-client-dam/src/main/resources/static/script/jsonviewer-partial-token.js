7a2d52287e79a38721cdd8436b46404ff71a457d
Claire JUILLARD <cjuillard@NX-DOMAIN.LAN>
2023-09-20 15:24:43 +0200

    demo-client-dam pages improvement

--- api-client-side-components/demo-client-dam/src/main/resources/static/script/jsonviewer-partial-token.js
+++ api-client-side-components/demo-client-dam/src/main/resources/static/script/jsonviewer-partial-token.js
@@ -4,14 +4,16 @@
 $(document).ready(function() {                
                 const header=getData("#header");
                 const body=getData("#body");
+                const btnTxt = "Détails du jeton Pro Santé Connect";            
+                if ( $( "#pageDAM" ).length ) {	 
+    			btnTxt="Détails du jeton ";
+                 }           
                 let jsonViewerHeader = new JSONViewer();    
                 let jsonViewerBody = new JSONViewer();
                    document.querySelector("#json-header").appendChild(jsonViewerHeader.getContainer());                           
     jsonViewerHeader.showJSON(JSON.parse(header), -1, 0);
     document.querySelector("#json-body").appendChild(jsonViewerBody.getContainer());                            
     jsonViewerBody.showJSON(JSON.parse(body), -1, 0); 
-//     var x = document.getElementById("details");
-//     x.style.visibility = "hidden";
        details();         
         });   
                      
@@ -23,16 +25,10 @@ function details() {
   var btn = document.getElementById("btnDetails");
   var x = document.getElementById("details");
   if (x.style.visibility === "hidden") {
-//	  alert("Les informations qui vont être affichées sont remontées dans le navigateur uniquement à titre de démonstration. En production, elles ne doivent pas sortir pas du proxy (\'Relying Party\')")
-	   btn.textContent="Masquer des détails techniques"
-    x.style.visibility = "visible";
-//    document.querySelector("#json-header").appendChild(jsonViewerHeader.getContainer());                           
-//    jsonViewerHeader.showJSON(JSON.parse(header), -1, -1);
-//    document.querySelector("#json-body").appendChild(jsonViewerBody.getContainer());                            
-//    jsonViewerBody.showJSON(JSON.parse(body), -1, -1); 
-   
+	   btn.textContent="Masquer le jeton"
+    x.style.visibility = "visible";   
   } else {
     x.style.visibility = "hidden";
-    btn.textContent="Voir des détails techniques"
+    btn.textContent=btnTxt;
   }
 }
