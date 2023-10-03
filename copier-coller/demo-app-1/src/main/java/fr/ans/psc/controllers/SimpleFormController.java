3ba5fefd8e9998da59ca5df5e5fb71bed4289e45
Claire JUILLARD <cjuillard@NX-DOMAIN.LAN>
2023-10-03 12:09:12 +0200

    copier-coller gestion api_token

--- copier-coller/demo-app-1/src/main/java/fr/ans/psc/controllers/SimpleFormController.java
+++ copier-coller/demo-app-1/src/main/java/fr/ans/psc/controllers/SimpleFormController.java
@@ -1,7 +1,12 @@
 package fr.ans.psc.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.cookie.Cookie;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.util.WebUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 public class SimpleFormController {
 
 	@GetMapping("/patient/form")
-	public String getSimpleFormPage() {
+	public String getSimpleFormPage(HttpServletRequest request) {
 		log.info("SimpleFormController ..");
+		javax.servlet.http.Cookie co = WebUtils.getCookie(request, "sts-token");
+		String re = ((co == null) ? "not found": co.getValue());
+		log.info("sts_token: {}", re);		
 		return "form-page";
 	}
 }
