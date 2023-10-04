aa19b83d8953fff80f74f2074bcf690caadaddcd
Claire JUILLARD <cjuillard@NX-DOMAIN.LAN>
2023-10-04 11:04:01 +0200

    copier-coller app1

--- copier-coller/demo-app-1/src/main/java/fr/ans/psc/controllers/PatientRecordingController.java
+++ copier-coller/demo-app-1/src/main/java/fr/ans/psc/controllers/PatientRecordingController.java
@@ -1,8 +1,12 @@
 package fr.ans.psc.controllers;
 
-import org.springframework.http.HttpHeaders;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,10 +16,20 @@ public class PatientRecordingController {
 
 	@GetMapping("/patient/recorded")
 //	public String navigate(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody String jsonSchema) {
-	public String navigate() {
-		log.debug("PatientRecordingController ..");
-//		log.debug("token: {}", token);
-//		log.debug("body: {}", jsonSchema);
+	public String navigate(HttpServletRequest request) {
+		log.info("PatientRecordingController ..");
+		logRequestHeaders(request);
 		return "recording-ok";
+	}
+
+	public void logRequestHeaders(HttpServletRequest request) {
+
+		Enumeration<String> headersName = request.getHeaderNames();
+		Iterator<String> iHeaderNames = headersName.asIterator();
+		while (iHeaderNames.hasNext()) {
+			String tmp = iHeaderNames.next();
+			String value = request.getHeader(tmp);
+			log.error("\t" + tmp + ": " + value);
+		}
 	}
 }
